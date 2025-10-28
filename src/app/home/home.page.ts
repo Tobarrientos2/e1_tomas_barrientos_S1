c3ee7fa09364bd1892f07b0bb85649e20aebf0b4
pyme0 <tom3bs@gmail.com>
2025-10-27 23:52:03 -0300

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import {inject} from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+
+
+
+  private fb: FormBuilder = inject(FormBuilder)
+
+  form: FormGroup = this.fb.group({
+    email: ['', [Validators.required], [Validators.email ]],
+    password: ['', [Validators.required], [Validators.minLength(6)]],
+  })
+  
+    onSubmit():void{
+      if(this.form.invalid) return;
+      console.log(this.form.value)
+    }
 
   constructor() {}
 
